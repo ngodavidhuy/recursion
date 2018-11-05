c746e66f72e95e2131ae34f45929650c74965b40
ngodavidhuy <ngodavidhuy@gmail.com>
2018-11-05 11:58:41 -0800

    Updated getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,26 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  let result = [];
+  let body = document.body;
+  
+  const helper = function (element) {
+    if (element.classList && element.classList.contains(className)) {
+      result.push(element);
+    }
+    
+    if (element.childNodes) {
+      for (let i = 0; i < element.childNodes.length; i++) {
+        helper(element.childNodes[i]);
+      }
+    }
+  };
+
+  // for (let i = 0; i < body.childNodes.length; i++) {
+    
+  // }
+  
+  helper(body);
+  
+  return result;
 };
